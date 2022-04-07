8235aceebd6508623adb00a951ba48905bb95bcd
VanTej <vanessatejedor@gmail.com>
2022-04-07 19:50:56 +0200

    Twig quest 3

--- public/details.php
+++ public/details.php
@@ -4,8 +4,5 @@
 require_once __DIR__ . '/../config/twig.php';
 require_once __DIR__. '/../vendor/autoload.php';
 
-$name = 'Vanessa';
 
-echo $twig->render('hello.html.twig', ['name' => $name]);
-
-
+echo $twig->render('details.html.twig');
---
+++ public/person.php
@@ -0,0 +1,14 @@
+<?php
+
+// Get a $twig object from this file.
+require_once __DIR__ . '/../config/twig.php';
+require_once __DIR__. '/../vendor/autoload.php';
+
+$persons = [
+    ['name' => 'riri', 'age' => 10],
+    ['name' => 'fifi', 'age' => 20],
+    ['name' => 'loulou', 'age' => 30],
+];
+
+echo $twig->render('person.html.twig', ['persons' => $persons]);
+
